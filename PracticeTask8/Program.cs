12e5a2a47bcb7896ba3d35795d23ad43ca6a925c
Alexey Subbotin <subbotin910@gmail.com>
2018-05-01 11:49:58 +0500

    initial commit. start of work

---
+++ PracticeTask8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeTask8
+{
+    class Program
+    {
+        // Task: find a clique of K size in a given graph.
+        // Input: the list of vertexes, the list of lines and the size of a clique.
+        // Student: Alexey Subbotin. Group: SE-17-1.
+        static void Main(string[] args)
+        {
+            // Getting the list of vertexes.
+            string vertexes = Console.ReadLine();
+
+            // Getting the list of lines.
+            string linesInput = Console.ReadLine();
+
+            // Creating the array of lines.
+            string[] lines = linesInput.Split(' ');
+
+            // Getting the size of a clique.
+            int K = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static string GetCombination()
+    }
+}
