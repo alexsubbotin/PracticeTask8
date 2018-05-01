66133ec990427f184800f8a52c0c0436b3a8c865
Alexey Subbotin <subbotin910@gmail.com>
2018-05-01 15:07:30 +0500

    comments added

--- PracticeTask8/Program.cs
+++ PracticeTask8/Program.cs
@@ -25,8 +25,10 @@ namespace PracticeTask8
             // Getting the size of a clique.
             int K = Convert.ToInt32(Console.ReadLine());
 
+            // It's impossible to look for a clique with the size greater than the number of vertexes.
             if (K <= vertexes.Length)
             {
+                // Start of recursion.
                 GetCombinations(lines, 0, lines.Length - K, "", K);
             }
             else
@@ -37,21 +39,33 @@ namespace PracticeTask8
             Console.ReadLine();
         }
 
+        // Function to go through all the possible combinations of lines.
+        // In order to optimise the search it goes like this (example for K=3 and 3 lines):
+        // 0 1
+        // 1 2
+        // 2 3
+        // Comnination will be: 012 013 123.
         public static string GetCombinations(string[] lines, int startIndex, int endIndex, string combination, int K)
         {
+            // Condition to go out of the recursion.
             if(endIndex >= lines.Length)
             {
+                // Analysing the created string of lines.
                 Analysis(combination, K);
                 return "";
             }
             else
             {
+                // Going through all the lines from startIndex to endIndex.
                 for(int i = startIndex; i <= endIndex; i++)
                 {
+                    // Adding the line.
                     combination += lines[i];
 
+                    // Going to the next tier.
                     combination += GetCombinations(lines, i + 1, endIndex + 1, combination, K);
 
+                    // After working with the next tier, the current line should be replaced with the next one.
                     combination = combination.Substring(0, combination.Length - 2);
                 }
 
@@ -59,18 +73,28 @@ namespace PracticeTask8
             }
         }
 
+        // Function to analyse the created list of lines.
+        // If lines create a clique the the number of different vertexes should be equal.
+        // For instance: ab ac bc, there are 2 of each letters.
+        // ab dc dc, the numbers of different letters are not equal – not a clique.
         public static void Analysis(string combination, int K)
         {
+            // Creating the array of letters (vertexes).
             char[] vertexes = combination.ToCharArray();
 
+            // Sorting it. So we get somthing like that: aabbcc (a clique).
             Array.Sort(vertexes);
 
+            // Indicates if it's a clique or not.
             bool ok = true;
 
+            // "Jumping" from letter to letter assuming it's a clique.
             for(int i = 0; i < vertexes.Length; i += K - 1)
             {
+                // Comparing letters in a group.
                 for(int j = i; j < i + K - 2; j++)
                 {
+                    // If some letters are not equal in the group then it doesn't look like aabbcc, then it's not a clique.
                     if(vertexes[j] != vertexes[j + 1])
                     {
                         ok = false;
@@ -82,6 +106,7 @@ namespace PracticeTask8
                     break;
             }
 
+            // If it's a clique then print it.
             if (ok)
             {
                 for(int i = 0; i < vertexes.Length; i += K - 1)
