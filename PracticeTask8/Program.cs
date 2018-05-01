0f77cc04fc3f28cc6bd353930153968bfb98cb04
Alexey Subbotin <subbotin910@gmail.com>
2018-05-01 17:03:13 +0500

    endIndex bug fixed

--- PracticeTask8/Program.cs
+++ PracticeTask8/Program.cs
@@ -28,15 +28,19 @@ namespace PracticeTask8
             // It's impossible to look for a clique with the size greater than the number of vertexes.
             if (K <= vertexes.Length)
             {
+                // The number of lines in a clique.
+                int numOfLines = K * (K - 1) / 2;
+
                 // Start of recursion.
-                GetCombinations(lines, 0, lines.Length - K, "", K);
+                GetCombinations(lines, 0, lines.Length - K, "", K, numOfLines);
 
                 if (allCliques == "")
                     Console.WriteLine("There are no cliques!");
                 else
                 {
                     // Printing all the found cliques.
-                    for(int i = 0; i < allCliques.Length; i++)
+                    Console.WriteLine("THE LIST OF CLIQUES:");
+                    for (int i = 0; i < allCliques.Length; i++)
                     {
                         if (allCliques[i] != ' ')
                             Console.Write(allCliques[i]);
@@ -61,11 +65,11 @@ namespace PracticeTask8
         // 0 1
         // 1 2
         // 2 3
-        // Comnination will be: 012 013 123.
-        public static string GetCombinations(string[] lines, int startIndex, int endIndex, string combination, int K)
+        // Combinations will be: 012 013 123.
+        public static string GetCombinations(string[] lines, int startIndex, int endIndex, string combination, int K, int numOfLines)
         {
             // Condition to go out of the recursion.
-            if(endIndex >= lines.Length)
+            if (numOfLines == 0)
             {
                 // Analysing the created string of lines.
                 Analysis(combination, K);
@@ -73,17 +77,22 @@ namespace PracticeTask8
             }
             else
             {
+                // if the endIndex went out of index range.
+                if (endIndex >= lines.Length)
+                    endIndex = lines.Length - 1;
+
                 // Going through all the lines from startIndex to endIndex.
-                for(int i = startIndex; i <= endIndex; i++)
+                for (int i = startIndex; i <= endIndex; i++)
                 {
                     // Adding the line.
                     combination += lines[i];
 
                     // Going to the next tier.
-                    combination += GetCombinations(lines, i + 1, endIndex + 1, combination, K);
+                    combination += GetCombinations(lines, i + 1, endIndex + 1, combination, K, numOfLines - 1);
 
                     // After working with the next tier, the current line should be replaced with the next one.
                     combination = combination.Substring(0, combination.Length - 2);
+
                 }
 
                 return "";
@@ -91,7 +100,7 @@ namespace PracticeTask8
         }
 
         // Function to analyse the created list of lines.
-        // If lines create a clique the the number of different vertexes should be equal.
+        // If lines create a clique then the number of different vertexes should be equal.
         // For instance: ab ac bc, there are 2 of each letters.
         // ab dc dc, the numbers of different letters are not equal – not a clique.
         public static void Analysis(string combination, int K)
@@ -99,20 +108,20 @@ namespace PracticeTask8
             // Creating the array of letters (vertexes).
             char[] vertexes = combination.ToCharArray();
 
-            // Sorting it. So we get somthing like that: aabbcc (a clique).
+            // Sorting it. So we get something like that: aabbcc (a clique).
             Array.Sort(vertexes);
 
             // Indicates if it's a clique or not.
             bool ok = true;
 
             // "Jumping" from letter to letter assuming it's a clique.
-            for(int i = 0; i < vertexes.Length; i += K - 1)
+            for (int i = 0; i < vertexes.Length; i += K - 1)
             {
                 // Comparing letters in a group.
-                for(int j = i; j < i + K - 2; j++)
+                for (int j = i; j < i + K - 2; j++)
                 {
                     // If some letters are not equal in the group then it doesn't look like aabbcc, then it's not a clique.
-                    if(vertexes[j] != vertexes[j + 1])
+                    if (vertexes[j] != vertexes[j + 1])
                     {
                         ok = false;
                         break;
@@ -126,13 +135,13 @@ namespace PracticeTask8
             // If it's a clique then store it.
             if (ok)
             {
-                for(int i = 0; i < vertexes.Length; i += K - 1)
+                for (int i = 0; i < vertexes.Length; i += K - 1)
                 {
                     allCliques += vertexes[i];
                 }
                 allCliques += " ";
             }
-            
+
         }
     }
 }
